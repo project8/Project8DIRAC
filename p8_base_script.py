ecdaba1ed8418d76483b566bb0926881d380def7
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-10-13 13:04:15 -0700

    Some formatting and debugging

--- p8_base_script.py
+++ p8_base_script.py
@@ -5,6 +5,7 @@
 from DIRAC import S_OK, S_ERROR, gLogger, exit as DIRAC_Exit
 from DIRAC.Core.Base import Script
 
+
 class BaseScript(object):
     # switches is a list of 3 or 4 element tuples, where the elements are:
     #      0 : short form (single character) command line flag
@@ -21,13 +22,18 @@ class BaseScript(object):
     #     Note that in the implementation of self.main() any configured switches show
     #         up in kwargs, positional arguments are in args
     switches = []
+    usage = []
 
     def __init__(self):
         '''
         '''
         for switch in self.switches:
             self.registerSwitch(switch)
-        Script.setUsageMessage(self.__doc__)
+        # Script.setUsageMessage(self.__doc__)
+        usageDoc = []
+        for aUsage in self.usage:
+            usageDoc.append(str(aUsage))
+        Script.setUsageMessage('\n'.join(usageDoc))
         Script.parseCommandLine(ignoreErrors=False)
         self.args = Script.getPositionalArgs()
 
@@ -36,12 +42,13 @@ class BaseScript(object):
         if hasattr(self, 'main'):
             self.main()
         else:
-            gLogger.error('no main method implemented, your custom logic goes there')
+            gLogger.error(
+                'no main method implemented, your custom logic goes there')
             DIRACExit(1)
         DIRAC_Exit(0)
 
     def _default_set(self, name, value):
-        gLogger.info('calling set {} -> {}'.format(name,repr(value)))
+        gLogger.info('calling set {} -> {}'.format(name, repr(value)))
         # This is so ugly, they set to '' when a flag is there with no value
         #   ... '' is logical False in python! grrrr (BHL)
         if value is '':
@@ -53,18 +60,25 @@ class BaseScript(object):
         this_name = switch[1].replace('-', '_').rstrip(':=')
 
         # if there isn't a setter method, use default
-        if not hasattr(self, 'set_'+this_name):
-            gLogger.debug('configuring default setter for <{}>'.format(this_name))
-            setattr(self, 'set_'+this_name, lambda x: self._default_set(this_name, x))
+        if not hasattr(self, 'set_' + this_name):
+            gLogger.debug(
+                'configuring default setter for <{}>'.format(this_name))
+            setattr(self, 'set_' + this_name,
+                    lambda x: self._default_set(this_name, x))
         try:
-            setter = getattr(self, 'set_'+this_name)
-            getattr(self, 'set_'+this_name)(None)
+            setter = getattr(self, 'set_' + this_name)
+            getattr(self, 'set_' + this_name)(None)
         except:
-            gLogger.error('unable to call {}'.format('set_'+this_name))
+            gLogger.error('unable to call {}'.format('set_' + this_name))
             pass
 
-        Script.registerSwitch(switch[0], switch[1], switch[2], getattr(self, 'set_'+this_name))
+        shortName = switch[0].rstrip(':=')
+        shortName += ":"
+        longName = switch[1].rstrip(':=')
+        longName += "="
+        Script.registerSwitch(
+            shortName, longName, switch[2], getattr(self, 'set_' + this_name))
 
         # if a default was provided, set it
         if len(switch) == 4:
-            getattr(self, 'set_'+this_name)(switch[3])
+            getattr(self, 'set_' + this_name)(switch[3])
