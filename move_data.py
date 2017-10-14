c2c5e1d2739d40cfd52bbaa9c8e71eabc7bb70e5
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-10-13 18:27:33 -0700

    Created job_submitter for moving data in the FC

---
+++ move_data.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python
+
+# Move data:
+# It takes an input file as input containing a dictionary of the folders to move and their new location.
+# For each subfolder to move, it will first download the files on the node, then add them to their new location on the FC and finally remove the files from the FC
+# NB: this script does not protect the genealogy of the files (yet...)
+# Author: M Guigue
+# Creation: Oct 10 2017
+
+
+import argparse
+import json
+import os
+
+from DIRAC.Interfaces.API.Dirac import Dirac
+dirac = Dirac()
+
+
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+fcc = FileCatalogClient()
+
+se = 'PNNL-HEP-SRM-SE'
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Move files in the FC (does not preserve ancestry)')
+    parser.add_argument(
+        'filename', help='Name of the file containing the location of the files to move and where')
+    args = parser.parse_args()
+
+    filename = args.filename
+    print("Move_Data: reading input file {}".format(filename))
+    import yaml
+    with open(filename, 'r') as stream:
+        try:
+            theDict = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    for aKey in theDict.keys():
+        print("Move_Data: Doing folder {}".format(aKey))
+        aDict = theDict[aKey]
+        for infile, outfile in aDict.iteritems():
+            print(infile, outfile)
+            status = dirac.getFile(infile)
+            if not status['OK']:
+                print("Move_Data: failed getting file ")
+                return
+            infilename = os.path.basename(filename)
+            if not os.path.exists(infilename):
+                print("Move_Data: file does not exist")
+                return
+            print("Move_Data: Successfully downloaded file")
+
+            status = dirac.addFile(outfile, infilename, se)
+            if not status['OK']:
+                print("Move_Data: failed uploading file")
+                return
+            print("Move_Data: Successfully uploaded file")
+
+            status = dirac.removeFile(infile)
+            if not status['OK']:
+                print("Move_Data: failed removing file on FC")
+                return
+            print("Move_Data: Successfully removed file on FC")
+
+            os.remove(infilename)
+            if os.path.exists(infilename):
+                print("Move_Data: file is still here")
+                return
+            print("Move_Data: Successfully removed local file")
+        print('Moved_Data: making sure it is empty first')
+        lfnlist = sorted([lfn for lfn in
+                          fcc.findFilesByMetadata(
+                              {}, path=aKey)['Value']
+                          ])
+        if lfn.len() != 0:
+            print("Move_Data: folder is not empty")
+            return
+        print("Move_Data: done with this folder -> removing it")
+        fcc.removeDir(aKey)
+        print("Move_Data: removed directory")
+
+
+if __name__ == "__main__":
+    main()
---
+++ move_data.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "hostname:" `/bin/hostname`
+echo " "; echo " "
+
+# DIRAC has the default value /opt/dirac/pro which does not exist in the
+# cluster environment.  You MUST unset DIRAC before running setup_katydid.sh
+# (it only sets the correct value if the current value is empty).
+unset DIRAC
+
+source /cvmfs/hep.pnnl.gov/project8/katydid-2.7.0/setup_katydid.sh
+echo " "; echo " "
+
+echo " "; echo " "
+echo "pre-job directory list:"
+ls -l
+echo " "; echo " "
+
+echo "command: python move_data.py $@"
+echo " "; echo " "
+python move_data.py $@
+
+echo " "; echo " "
+echo "post-job directory list:"
+ls -l
+
+
