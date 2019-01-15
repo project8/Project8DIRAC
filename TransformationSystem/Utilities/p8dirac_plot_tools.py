2b27779365255b11aff2d4a1de6aa65043475a92
thom991 <mathew.thomas@pnnl.gov>
2019-01-15 12:21:24 -0800

    Update p8dirac_plot_tools.py

--- TransformationSystem/Utilities/p8dirac_plot_tools.py
+++ TransformationSystem/Utilities/p8dirac_plot_tools.py
@@ -56,7 +56,8 @@ PROD_DEST_DATA_SE = ops_dict.get('ProdDestDataSE', 'PNNL-PIC-SRM-SE')
 PROD_DEST_MONITORING_SE = ops_dict.get('ProdDestMonitoringSE', '')
 
 def check_lfn_health(lfn, software_tag):
-    status = os.system("source /cvmfs/hep.pnnl.gov/project8/katydid/" + "v2.13.0" + "/setup.sh\nroot -b " + lfn + " -q")
+    #status = os.system("source /cvmfs/hep.pnnl.gov/project8/katydid/" + "v2.13.0" + "/setup.sh\nroot -b " + lfn + " -q")
+    status = os.system("source /cvmfs/hep.pnnl.gov/project8/common/v0.4.0/setup.sh\nroot -b " + pfn + " -q")
     return status
   
 def getPlotJobLFNs():
